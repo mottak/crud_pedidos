214d84e28bc4593ba973f269af9726bf587624c7
mottak <kissylamottapereira@gmail.com>
2022-08-07 18:12:10 -0300

    configurando react-router-dom

---
+++ front-end/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <main>
+      <h2>Login Page</h2>
+    </main>
+  );
+}
+
+export default Login;
---
+++ front-end/src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <main>
+      <h2>Register Page</h2>
+    </main>
+  );
+}
+
+export default Register;
