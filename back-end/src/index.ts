22e258439117635c4c12a4411b86cc4d6ace0d85
mottak <kissylamottapereira@gmail.com>
2022-08-13 10:03:11 -0300

    instalando cors

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
 import errorMiddleware from './middlewares/error';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
