36e7fcb0753f934490d9d5093b9a4a1fb1f51618
mottak <kissylamottapereira@gmail.com>
2022-08-22 09:54:41 -0300

    construindo seeders

---
+++ back-end/seeders.sql
@@ -0,0 +1,33 @@
+INSERT INTO
+    ordersDatabase.users (id, name, email, password, role)
+VALUES (
+        'bdcb4d42-4449-470e-ba88-4a3b6c290383',
+        'Zé da Silva',
+        'zesilva@email.com',
+        '123456',
+        'client'
+    ), (
+        'bdcb4d42-4449-470e-ba88-4a3b6c290383',
+        "João dono da loja",
+        "joao@email.com",
+        "123456",
+        "seller"
+    )
+INSERT INTO
+    ordersDatabase.products (id, name, quantity, price)
+VALUES (
+        "5a9df949-3e28-4865-bd19-260b9cef4754",
+        "lápis",
+        20,
+        0.5
+    ), (
+        "ba18fc37-c7b2-48ac-82f1-9bf161d396f2",
+        "borracha",
+        10,
+        1.2
+    ), (
+        "53ee23dc-d502-456c-a93f-1e4b6646bd41",
+        "caderno",
+        50,
+        18.8
+    )
