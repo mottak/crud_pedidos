1a611dc3540ab4f08bef1a2b22cdd4767bb49e22
mottak <kissylamottapereira@gmail.com>
2022-09-20 11:53:51 -0300

    alterando status code rota put product

--- back-end/src/main/routes/api/products.routes.ts
+++ back-end/src/main/routes/api/products.routes.ts
@@ -26,7 +26,7 @@ productRoutes.put('/products/:id', async (req, res) => {
   const dataId = await idSchema.validateAsync(req.params)
   const dataBody = await productSchema.validateAsync(req.body)
   const result = await productFactory().update(req.headers.authorization, dataId.id, dataBody)
-  return res.status(201).json(result)
+  return res.status(200).json(result)
 })
 
 
