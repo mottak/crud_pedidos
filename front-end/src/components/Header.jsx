18d2bb07c00b3b7e53c48c657c96aff1579da326
Kissyla <kissylamottapereira@gmail.com>
2022-11-24 16:09:52 -0300

    add botão de sair ao header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './Header.css';
 
 function Header({ title }) {
   const userStorage = localStorage.getItem('user');
   const user = JSON.parse(userStorage);
+
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    localStorage.removeItem('user');
+    navigate('../', { replace: true });
+  };
   return (
     <div>
       <h3>{title}</h3>
       <h6>{`Bem vindo ${user.userName}`}</h6>
+      <button
+        className="btn btn-secondary"
+        type="button"
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        sair
+      </button>
     </div>
   );
 }
