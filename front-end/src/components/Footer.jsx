1a83b1ea63c7250070655338a2ac35067e986353
Kissyla <kissylamottapereira@gmail.com>
2022-11-24 15:51:44 -0300

    criando footer

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Fotter.css';
+
+function Footer() {
+  return (
+    <div>
+      <p>Feito com React, css, typescript e carinho 💌</p>
+    </div>
+  );
+}
+
+export default Footer;
