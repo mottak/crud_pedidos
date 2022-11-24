88aa8a486fa5251c922ce808c429c0096e6e1a7b
Kissyla <kissylamottapereira@gmail.com>
2022-11-24 15:26:25 -0300

    renomeando pagina de produtos de clientes

--- front-end/src/pages/client/OrdersClientPage.jsx
+++
@@ -1,9 +0,0 @@
-function ClientOrder() {
-  return (
-    <main>
-      <h2>clientOrder Page</h2>
-    </main>
-  );
-}
-
-export default ClientOrder;
---
+++ front-end/src/pages/client/ProductsClientPage.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+
+function ClientProducts() {
+  return (
+    <main>
+      <Header title="Products" />
+    </main>
+  );
+}
+
+export default ClientProducts;
