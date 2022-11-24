17c628e0d6c2d68a6ce3add65eae8daf3234718d
Kissyla <kissylamottapereira@gmail.com>
2022-11-24 15:29:55 -0300

    criando header

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,7 +1,13 @@
+import React, { useContext } from 'react';
+import UserContext from '../context/userContext/UserContext';
+import './Header.css';
+
 function Header({ title }) {
+  const { userName } = useContext(UserContext);
   return (
     <div>
       <h3>{title}</h3>
+      <h6>{`Bem vindo ${userName}`}</h6>
     </div>
   );
 }
