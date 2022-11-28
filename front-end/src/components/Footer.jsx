75071c8f07c6ecaa8b07379908047ab4eb5e5987
Kissyla <kissylamottapereira@gmail.com>
2022-11-28 15:41:01 -0300

    centralizando footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -4,13 +4,12 @@ function Footer() {
   return (
     <footer className="footer position-absolute bottom-2 py-2 w-100">
       <div className="container">
-        <div className="row align-items-center justify-content-lg-between">
+        <div className="row justify-content-lg-center">
           <div className="col-12 col-md-6 my-auto">
-            <div className="copyright text-center text-sm text-lg-start">
+            <div className="copyright text-sm text-lg-center">
               Feito com React, material-dashboard, typescript e carinho 💌
             </div>
           </div>
-          <div className="col-12 col-md-6" />
         </div>
       </div>
     </footer>
